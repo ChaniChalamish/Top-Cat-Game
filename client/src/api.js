86e84cbb13e4faae2965177b676b5b03619f4414
Shkedi Chana <117741319+ChaniChalamish@users.noreply.github.com>
2025-10-19 11:51:47 +0300

    Update API base URL to production endpoint

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:5000/api/leaderboard" });
+const API = axios.create({ baseURL: "https://top-cat-game-uujg.onrender.com/api/leaderboard" });
 
 export const getTopUsers = (n = 10) => API.get(`/top/${n}`);
